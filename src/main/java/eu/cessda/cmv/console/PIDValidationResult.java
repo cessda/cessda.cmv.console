dfe6f98f7066961eecc7223b4ab57bb2dd33e72b
Matthew Morris <matthew.morris@cessda.eu>
2024-03-25 11:16:12 +0100

    #635: Report details about valid persistent identifiers in addition to invalid persistent identifiers
    This commit also caches the XPath instance used to compile XPaths.

--- src/main/java/eu/cessda/cmv/console/PIDValidationResult.java
+++ src/main/java/eu/cessda/cmv/console/PIDValidationResult.java
@@ -17,5 +17,5 @@ package eu.cessda.cmv.console;
 
 import java.util.List;
 
-record PIDValidationResult(boolean valid, List<PID> invalidPIDs) {
+record PIDValidationResult(boolean valid, List<PID> validPIDs, List<PID> invalidPIDs) {
 }
